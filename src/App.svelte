202cac3f2ce649964e21b3e872f7cf08b4076af0
Denis Perchenko <plachenko@gmail.com>
2023-10-07 16:15:44 -0400

    adding timeline scrubber

--- src/App.svelte
+++ src/App.svelte
@@ -1,74 +1,55 @@
 <script>
-  import { Piano, onKeyDown, onKeyUp } from "svelte-piano";
+  import { Piano, onKeyDown, onKeyUp} from 'svelte-piano';
   import { onMount } from "svelte";
+  import Timeline from './timeline.svelte';
 
   let socket;
+  let open = false;
   let host;
 
-  let open = false;
-  $: {
-    sendSocket(
-      JSON.stringify({
-        type: "note",
-        obj: {
-          note: $onKeyUp?.note,
-          vel: $onKeyUp?.velocity,
-        },
-      })
-    );
-  }
-  $: {
-    sendSocket(
-      JSON.stringify({
-        type: "note",
-        obj: {
-          note: $onKeyDown?.note,
-          vel: $onKeyDown?.velocity,
-        },
-      })
-    );
-  }
-  // $: console.log($onKeyDown)
+  $: sendNote($onKeyDown)
+  $: sendNote($onKeyUp)
 
-  function sendSocket(d) {
-    if (open) {
-      socket.send(d);
+  function sendNote(note){
+    if(!note) return;
+
+    const obj = {
+      type: 'note',
+      obj: {
+        note: note.note,
+        vel: note.velocity
+      }
     }
+    sendSocket(obj);
+  }
+  
+  function sendSocket(d){
+      socket.send(JSON.stringify(d));
+  }
+
+  function setSocket(_host){
+    socket = new WebSocket(`ws://${_host}:8081`);
   }
 
   onMount(() => {
     host = window.location.hostname;
     setSocket(host);
-  });
-
-  function setSocket(host) {
-    socket = new WebSocket("ws://" + host + ":8081");
-    socket.addEventListener("open", (e) => {
+    socket.addEventListener('open', (e) => {
       open = true;
     });
+  });
+  function tits(f){
+    console.log(f);
   }
 </script>
 
 <main>
   <input type="text" bind:value={host} />
   <div on:click={setSocket(host)}>connect</div>
-  <Piano --width="900px" --height="300px" />
+  <Timeline  />
+  <Piano on:onmousedown={() => tits('hi')} --width="900px" --height="300px" />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
---
+++ src/timeline.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { onMount } from "svelte";
+
+    let scrubber;
+    let left = 0;
+    /*
+    onMount(() => {
+        setInterval(() => {
+           scrubLeft(1);
+
+        }, 1000);
+    });
+    */
+
+    function scrubLeft(amt){
+        left += amt;
+        scrubber.style.left = `${left}px`;
+        scrubber.scrollIntoView();
+    }
+
+</script>
+
+<div class="timeline">
+    <div bind:this={scrubber} class="scrubber" />
+</div>
+
+<style>
+    .timeline{
+        width: 100%;
+        height: 100px;
+        background-color: #CCC;
+        position: relative;
+        flex: 1;
+        overflow: auto;
+    }
+
+    .scrubber{
+        background-color: #F00;
+        width: 2px;
+        height: 100%;
+        position: absolute;
+        left: 0px;
+        top: 0px;
+    }
+
+</style>
